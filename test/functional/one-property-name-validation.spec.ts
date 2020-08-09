4ba9d8cb2a40f497e70b7cba7434ec237446eb61
Mauricio Rivera <mauriciorivera@iMac-de-mac.local>
2020-08-09 16:02:52 -0500

    update: refactor of one property validation. Added package.build.json for npm publish. Updated README.md for one property validation.

--- test/functional/one-property-name-validation.spec.ts
+++ test/functional/one-property-name-validation.spec.ts
@@ -4,20 +4,29 @@ import { Validator } from '../../src/validation/Validator';
 const validator = new Validator();
 
 describe('one property name validation', () => {
+  class MyClass {
+    @IsNotEmpty()
+    title: string;
+
+    @IsNotEmpty()
+    text: string;
+  }
+
   it('should validate the property text, set on validator options, and show 1 error', () => {
     expect.assertions(1);
-
-    class MyClass {
-      @IsNotEmpty()
-      title: string;
-
-      @IsNotEmpty()
-      text: string;
-    }
 
     const model = new MyClass();
     return validator.validate(model, { propertyName: 'text' }).then(errors => {
       expect(errors.length).toEqual(1);
+    });
+  });
+
+  it('should validate all properties if property name is not set', () => {
+    expect.assertions(1);
+
+    const model = new MyClass();
+    return validator.validate(model).then(errors => {
+      expect(errors.length).toEqual(2);
     });
   });
 });
